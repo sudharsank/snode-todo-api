2cf4dd84bf73308330c18d7da774976c8e1ea820
Sudharsan K <sudharsan_1985@live.in>
2016-02-04 16:18:40 +0800

    Validated using underscore.js

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var port = process.env.PORT || 3000;
-
-//var todos = [{
-//    id: 1,
-//    description: 'Meet friend in the reception' ,
-//    completed: false
-//}, {
-//    id: 2,
-//    description: 'Deploy the solution updated',
-//    completed: false
-//}, {
-//    id: 3,
-//    description: 'Reach office',
-//    completed: true
-//}];
 
 var todos = [];
 var todoNextId = 1;
@@ -34,12 +22,7 @@ app.get('/todos', function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id, 10);
-    var matchedToDo;
-    todos.forEach(function(todo){
-       if(todoId === todo.id) {
-           matchedToDo = todo;
-       }
-    });
+    var matchedToDo = _.findWhere(todos, {id: todoId});
     
     if(matchedToDo){
         res.send(matchedToDo);        
@@ -50,20 +33,13 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos
 app.post('/todos', function(req, res){
-    var body = req.body;
-    
+    var body = _.pick(req.body, "description", "completed");// req.body; 
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+        return res.status(400).send();
+    }
+    body.description = body.description.trim();
     body.id = todoNextId++;
     todos.push(body);
-    
-//    todos.push({
-//        id: todoNextId,
-//        description: body.description,
-//        completed: body.completed
-//    });
-//    todoNextId = todoNextId+1;
-    
-    //console.log("description: " + body.description);
-    
     res.send(body);
 });
 
